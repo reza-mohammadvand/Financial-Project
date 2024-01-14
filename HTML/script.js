93dd7d71fbcafb1f646b277b93e07eae1e2c2f52
reza <r.mohammadvand98@gmail.com>
2024-01-14 19:25:10 +0330

    Web page development v3.0

--- HTML/script.js
+++ HTML/script.js
@@ -130,5 +130,97 @@ function hideAlertBox(count) {
 }
 
 
+// search box
+// Your list of items
+var list = [["AAPL","#tabs-3"],["GOOGL","#tabs-4"],["AMZN","#tabs-5"],["MSFT","#tabs-6"],["TSLA","#tabs-7"]];
+
+$(document).ready(function() {
+  $('#searchInput').on('input', function() {
+      var input = $(this).val().toLowerCase();
+
+      // If the input is empty, hide the list
+      if (input === '') {
+          $('#list').hide();
+          return;
+      }
+
+      var result = list.filter(item => item[0].toLowerCase().includes(input));
+
+      // Clear the current list
+      $('#list').empty();
+
+      // Add the search results to the list
+      result.forEach(item => {
+          var a = $('<a>').text(item[0]).attr('href', item[1]);
+          var li = $('<li>').append(a).hide().appendTo('#list').slideDown(200);
+      });
+
+      // Show the list
+      $('#list').show();
+  });
+});
+
+
+
+
+
+var numDataPoints = 10; // The number of data points
+var pointWidth = 80; // The width of each data point in px
+document.getElementById('myChart').width = numDataPoints * pointWidth;
+
+
+const myChart = new Chart("myChart", {
+  type: "line",
+  data: {
+    labels: ["2024-01-01", "2024-01-02", "2024-01-03", "2024-01-04", "2024-01-05","2024-01-01", "2024-01-02", "2024-01-03", "2024-01-04", "2024-01-05", "2024-01-05","2024-01-05", "2024-01-05"], // This will be populated with your time data
+    datasets: [{
+      label: 'price',
+      data: [120.1, 121.5, 119.7, 122.3, 123.5,120.1, 121.5, 119.7, 122.3, 123.5, 123.5, 75, 75], // This will be populated with your price data
+      borderColor: 'rgba(75,192,192,1)'
+    }, {
+      label: 'MA',
+      data: [120.1, 120.8, 120.43, 120.9, 121.42,120.1, 120.8, 120.43, 120.9, 121.42, 123.5, 75, 75], // This will be populated with your moving average data
+      borderColor: 'rgba(192,75,192,1)'
+    }, {
+      label: 'EMA',
+      data: [120.1, 120.93, 120.48, 121.27, 122.07,120.1, 120.93, 120.48, 121.27, 122.07, 123.5, 75, 75], // This will be populated with your EMA data
+      borderColor: 'rgba(192,192,75,1)'
+    }, {
+      label: 'RSI',
+      data: [70, 72, 69, 73, 75,70, 72, 69, 73, 75, 75, 75, 75], // This will be populated with your RSI data
+      borderColor: 'rgba(75,75,192,1)'
+    }]
+  },
+  options: {
+    responsive: false,
+    tooltips: {
+      mode: 'index',
+      intersect: false,
+      callbacks: {
+        label: function(tooltipItem, data) {
+          var label = data.datasets[tooltipItem.datasetIndex].label || '';
+          if (label) {
+            label += ': ';
+          }
+          label += tooltipItem.yLabel;
+          return label;
+        },
+        afterBody: function(tooltipItems, data) {
+          var tooltipItem = tooltipItems[0];
+          var afterBody = [];
+          var dataPoint = data.datasets[0].data[tooltipItem.index]; // Always refer to the first dataset
+          afterBody.push('Open: ' + dataPoint.open);
+          afterBody.push('High: ' + dataPoint.high);
+          afterBody.push('Low: ' + dataPoint.low);
+          afterBody.push('Volume: ' + dataPoint.volume);
+          return afterBody;
+        }
+      }
+    }
+  }
+  
+
+});
+
 
 
